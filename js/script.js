9ef3b1e1eeacb287d21733ea5f5f83cb7f713402
Franco <franco.ulzurrun.dmw@gmail.com>
2023-01-18 02:52:34 -0300

    actualizacion de tp

--- js/script.js
+++ js/script.js
@@ -5,7 +5,15 @@ let mangas = [
     { id: 48, nombre: "Blue Lock", categoria: "Deportes", precio: 1250, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/bluelock/bluelock01.jpg" },
     { id: 54, nombre: "Ao No Flag", categoria: "Slice of Life", precio: 1500, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/aonoflag/aonoflag01.jpg" },
     { id: 61, nombre: "Look Back", categoria: "Slice of Life", precio: 1250, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/lookback/lookback.jpg" },
+    { id: 64, nombre: "My Hero Academia", categoria: "Accion", precio: 1250, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/myheroacademia/myheroacademia01.jpg" },
+    { id: 66, nombre: "Evangelion", categoria: "Accion", precio: 1500, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/evangelion/evangeliondeluxe_01.jpg" },
+    { id: 11, nombre: "Oyasumi Pumpum", categoria: "Slice of Life", precio: 1500, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/oyasumipunpun/oyasumipunpun01.jpg" },
+    { id: 27, nombre: "Haikyu!!", categoria: "Deportes", precio: 1250, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/haikyu/haikyu01.jpg" },
+    { id: 47, nombre: "SpyXFamily", categoria: "Accion", precio: 1250, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/spyxfamily/spyxfamily01.jpg" },
+    { id: 47, nombre: "One Piece", categoria: "Accion", precio: 1250, marca: "Ivrea", rutaImagen: "https://www.ivrea.com.ar/onepiece/onepiece01.jpg" },
+
 ]
+
 
 let carro = []
 
@@ -48,8 +56,8 @@ function renderizarmangas(arrayDemangas) {
         let tarjeta = document.createElement("div")
         tarjeta.className = "tarjeta"
         tarjeta.innerHTML = `
-        <h5>${manga.nombre}</h5>
-        <h4>$${manga.precio}</h4>
+        <p>${manga.nombre}</p>
+        <p>$${manga.precio}</p>
         <img src=${manga.rutaImagen}>
         <button type="button" class="btn btn-outline-success" style= "margin-top: 20px" id=${manga.id}>Agregar al carrito</button>
       `
@@ -65,30 +73,63 @@ function renderizarmangas(arrayDemangas) {
 function agregarAlCarrito(e) {
     let mangaBuscado = mangas.find(manga => manga.id == e.target.id)
     let chequeoManga = carro.findIndex(manga => manga.id == mangaBuscado.id)
-    if(chequeoManga!=-1){
+    if (chequeoManga != -1) {
         carro[chequeoManga].cantidad++
-        carro[chequeoManga].precioTotalCantidad = carro[chequeoManga].cantidad*carro[chequeoManga].precio
+        carro[chequeoManga].precioTotalCantidad = carro[chequeoManga].cantidad * carro[chequeoManga].precio
     }
-    else{
-        carro.push({id: mangaBuscado.id, nombre: mangaBuscado.nombre, categoria: mangaBuscado.categoria, precio:mangaBuscado.precio, marca: mangaBuscado.marca, cantidad: 1, precioTotalCantidad:mangaBuscado.precio})
+    else {
+        carro.push({ id: mangaBuscado.id, nombre: mangaBuscado.nombre, categoria: mangaBuscado.categoria, precio: mangaBuscado.precio, marca: mangaBuscado.marca, cantidad: 1, precioTotalCantidad: mangaBuscado.precio })
     }
-    actualizarCarrito()
+    totalFinal = carro.reduce((a, b) => a + b.precioTotalCantidad, 0)
+    unidades = carro.reduce((a, b) => a + b.cantidad, 0)
+    actualizarCarrito(carro)
     console.log(carro)
 }
 
-let totalTotal = 0
-const actualizarCarrito = () => {
-    carrito.innerHTML = "<h3>Carrito</h3>"
-    carro.forEach((prod) => {
-        const div = document.createElement('div')
-        div.className = ('list-group')
-        div.innerHTML = `
-        <li class = "list-group-item">Nombre: ${prod.nombre}</li>
-        <li class = "list-group-item">Precio: ${prod.precioTotalCantidad}$</li>
-        <li class = "list-group-item">Cantidad: ${prod.cantidad}</li>
-        `
-        carrito.appendChild(div)
-    })
-    totalTotal = carro.reduce((acc, valorTotal) => acc + valorTotal.precioTotalCantidad, 0)
-    carrito.innerHTML += `Total Carrito: $${totalTotal}`
+function eliminarCarrito(e) {
+    let mangaBuscado = mangas.find(manga => manga.id == e.target.id)
+    let chequeoManga = carro.findIndex(manga => manga.id == mangaBuscado.id)
+    if (chequeoManga != -1) {
+        if (carro[chequeoManga].cantidad >= 2) {
+            carro[chequeoManga].cantidad--
+            carro[chequeoManga].precioTotalCantidad = carro[chequeoManga].precioTotalCantidad - carro[chequeoManga].precio
+        }
+        else {
+            carro.splice(chequeoManga, 1)
+        }
+    }
+    totalFinal = carro.reduce((a, b) => a + b.precioTotalCantidad, 0)
+    unidades = carro.reduce((a, b) => a + b.cantidad, 0)
+    actualizarCarrito(carro)
+    console.log(carro)
 }
+
+function actualizarCarrito(array) {
+    carrito.innerHTML = ""
+    carrito.style.display = "flex";
+    for (let manga of array) {
+        let { nombre, cantidad, precioTotalCantidad, id } = manga
+        let div = document.createElement('div')
+        div.className = "list-group"
+        div.innerHTML = `
+        <div class = "carrito_edicion">
+        <div style = "flex:2">${nombre}</div>
+        <div style = "flex:2">Precio: ${precioTotalCantidad}$</div>
+        <div style = "flex:2">Cantidad: ${cantidad}</div>
+        <button id="${id}"class = "restar list-group-item btn btn-outline-success"> - </button>
+        <button id="${id}"class = "sumar list-group-item btn btn-outline-success"> + </button>
+        </div>
+        `
+        carrito.append(div)
+
+        let restar = document.getElementsByClassName("restar list-group-item btn btn-outline-success")
+        for (let a of restar) {
+            a.addEventListener("click", eliminarCarrito)
+        }
+        let sumar = document.getElementsByClassName("sumar list-group-item btn btn-outline-success")
+        for (let b of sumar) {
+            b.addEventListener("click", agregarAlCarrito)
+        }
+    }
+}
+
